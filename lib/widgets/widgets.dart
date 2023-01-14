4d8c3b013b2db7fb18dd0399708fbccaca32fc73
afthalad <afthalad8@gmail.com>
2023-01-14 20:33:54 +0530

    Ask the imam

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -2081,6 +2081,51 @@ class QuestionsInnerScreen extends StatelessWidget {
   }
 }
 
+//Ask the imam - Categories in screen
+class AskTheImamCategoriesScreen extends StatelessWidget {
+  final String appBarTitle;
+  final Widget pageWidget;
+  String? catDescription;
+  AskTheImamCategoriesScreen({
+    super.key,
+    required this.appBarTitle,
+    required this.pageWidget,
+    this.catDescription,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          elevation: 1,
+          backgroundColor: const Color(0xFF66B35A),
+          centerTitle: true,
+          title: Text(appBarTitle),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ListTile(
+                title: Text(
+                  '${appBarTitle} question and answer ',
+                  style: TextStyle(fontSize: 14, height: 3),
+                ),
+                subtitle: Text(
+                  catDescription.toString(),
+                  style: TextStyle(
+                    fontSize: 12,
+                  ),
+                ),
+              ),
+              Expanded(child: pageWidget),
+            ],
+          ),
+        ));
+  }
+}
+
 //ASk the imam - Ask question screen
 class AskTheQuestionScreen extends StatefulWidget {
   const AskTheQuestionScreen({
