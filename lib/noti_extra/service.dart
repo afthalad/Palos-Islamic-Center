46d49797343e72752ab46a19cace2e4e34527809
afthalad <afthalad8@gmail.com>
2023-01-21 10:54:18 +0530

    Add notification foldren package disabled in pubspec.yaml

--- lib/noti_extra/service.dart
+++ lib/noti_extra/service.dart
@@ -1,24 +1,13 @@
-// ignore_for_file: unnecessary_new
-
-import 'dart:async';
-import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/data/latest_all.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
+import 'dart:async';
 
-void main() => runApp(new MaterialApp(home: new MyApp()));
+class notify {
+  notifyInitialize() {
+    FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-      FlutterLocalNotificationsPlugin();
-
-  void initState() {
-    super.initState();
     const AndroidInitializationSettings initializationSettingsAndroid =
         AndroidInitializationSettings('@mipmap/ic_launcher');
 
@@ -31,25 +20,10 @@ class _MyAppState extends State<MyApp> {
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: new AppBar(
-        title: new Text('Flutter Local Notification'),
-      ),
-      body: new Center(
-        child: new ElevatedButton(
-          onPressed: showNotification,
-          child: new Text(
-            'Demo',
-          ),
-        ),
-      ),
-    );
-  }
-
   showNotification() async {
-    String locationName = 'America/New_York';
+    FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
+    String locationName = 'Asia/Colombo';
 
     tz.initializeTimeZones();
     tz.setLocalLocation(tz.getLocation(locationName));
@@ -67,8 +41,8 @@ class _MyAppState extends State<MyApp> {
       0,
       'scheduled title',
       'scheduled body',
-      tz.TZDateTime.now(tz.local).add(const Duration(seconds: 5)),
-      NotificationDetails(
+      tz.TZDateTime.now(tz.local).add(const Duration(seconds: 0)),
+      const NotificationDetails(
         android: AndroidNotificationDetails(
           'your channel id',
           'your channel name',
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,6 @@ import FlutterMacOS
 import Foundation
 
 import audio_session
-import flutter_local_notifications
 import geolocator_apple
 import just_audio
 import path_provider_macos
@@ -16,7 +15,6 @@ import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
-  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
